78e1dcc270b5cfbd94a109fa2384ba3cacfaf40a
Karol Kaczmarek <KarolKaczmarek@users.noreply.github.com>
2016-04-22 15:23:55 -0700

    Adding tests for Get-GitHubIssuesForRepository

--- Tests/GitHubAnalytics.tests.ps1
+++ Tests/GitHubAnalytics.tests.ps1
@@ -8,7 +8,12 @@
 [String] $root = Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path)
 Import-Module (Join-Path -Path $root -ChildPath 'GitHubAnalytics.psm1') -Force
 
-$script:repositoryUrl = "https://github.com/KarolKaczmarek/TestRepository"
+$script:gitHubAccountUrl = "https://github.com/KarolKaczmarek"
+$script:repositoryName = "TestRepository"
+$script:repository2Name = "TestRepository2"
+$script:repositoryUrl = "$script:gitHubAccountUrl/$script:repositoryName"
+$script:repositoryUrl2 = "$script:gitHubAccountUrl/$script:repository2Name"
+
 
 Describe 'Obtaininig issues for repository' {
     Context 'When no addional conditions specified' {
@@ -16,6 +21,32 @@ Describe 'Obtaininig issues for repository' {
 
         It 'Should return expected number of issues' {
             @($issues).Count | Should be 3
+        }
+    }
+    
+    Context 'When state and time range specified' {
+        $issues = Get-GitHubIssuesForRepository `
+            -repositoryUrl @($repositoryUrl) `
+            -createdOnOrAfter '2016-04-10'
+
+        It 'Should return expected number of issues' {
+            @($issues).Count | Should be 1
+        }
+    }
+}
+
+Describe 'Obtaininig repository with biggest number of issues' {
+    Context 'When no addional conditions specified' {
+        $issues = Get-GitHubTopIssuesRepository -repositoryUrl @($script:repositoryUrl,$script:repositoryUrl2)
+
+        It 'Should return expected number of issues for each repository' {
+            @($issues[0].Value) | Should be 3
+            @($issues[1].Value) | Should be 2
+        }
+        
+        It 'Should return repositories in descending order' {
+            @($issues[0].Name) | Should be $script:repositoryName
+            @($issues[1].Name) | Should be $script:repository2Name
         }
     }
     
